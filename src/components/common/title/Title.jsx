fa075c5875a2e73ebc31a578202a689db4741826
Pavlo Vovzhynskyy <pavlovovzhynskyy@MacBook-Air-Pavlo.local>
2023-03-01 02:30:40 +0100

    add global fonts, why us done

---
+++ src/components/common/title/Title.jsx
@@ -0,0 +1,13 @@
+import localFont from "@next/font/local";
+
+import styles from "./Title.module.scss";
+
+const ermilov = localFont({
+	src: "../../../../public/fonts/ermilov-bold-webfont.woff2",
+});
+
+const Title = ({ title }) => {
+	return <h2 className={`${styles.title} ${ermilov.className}`}>{title}</h2>;
+};
+
+export default Title;
