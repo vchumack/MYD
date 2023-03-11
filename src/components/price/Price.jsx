87ad2a56fd26c110565ddd98be5ff7c67a3cad00
Valeriya Druchinina <vchumack@glmedia.org>
2023-03-11 06:49:29 +0200

    added the stages section and markup of the rates section

--- src/components/price/Price.jsx
+++ src/components/price/Price.jsx
@@ -1,13 +1,47 @@
 import { useTranslation } from "next-i18next";
+import Title from "../common/title/Title";
+import { prices } from "@/data/price";
 
 import styles from "./Price.module.scss";
 
 const Price = () => {
-	const { t } = useTranslation("footer");
+	const { t } = useTranslation("price");
+
+	function Rate({ name, description, bonus, price, design, idx }) {
+		return (
+			<div>
+				<h3>{t(`cards.title.${idx + 1}`)}</h3>
+				<ul>
+					{description.map((item, index) => (
+						<li key={index}>
+							{t(`cards.list.${idx + 1}.${index + 1}`)}
+						</li>
+					))}
+				</ul>
+				<p>{bonus && t(`cards.bonus.${idx + 1}`)}</p>
+				<p>{t(`cards.price.${idx + 1}`)}</p>
+				<p>{t(`cards.design.${idx + 1}`)}</p>
+
+				<button>{t("btn")}</button>
+			</div>
+		);
+	}
 
 	return (
 		<div className="section">
-			<div className="container">{t("title")}</div>
+			<div className="container">
+				<Title title={t("title")} />
+				<ul>
+					{prices.map((item, i) => (
+						<Rate
+							key={i}
+							idx={i}
+							{...item}
+							design={t(`cards.design.${item.design}`)}
+						/>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 };
--- src/components/steps/Steps.jsx
+++ src/components/steps/Steps.jsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "next-i18next";
+import Title from "../common/title/Title";
 
 import styles from "./Steps.module.scss";
 
@@ -7,9 +8,73 @@ const Steps = () => {
 
 	return (
 		<div className="section">
-			<div className="container">{t("title")}</div>
+			<div className="container">
+				<Title title={t("title")} />
+				<div className={styles.grid}>
+					<p className={`${styles.textLeft} ${styles.text1}`}>
+						{t("step1")}
+					</p>
+					<div className={styles.skip1}></div>
+					<div className={styles.skip2}></div>
+					<p className={`${styles.textRight} ${styles.text3}`}>
+						{t("step2")}
+					</p>
+					<p className={`${styles.textLeft} ${styles.text2}`}>
+						{t("step3")}
+					</p>
+					<div className={styles.skip3}></div>
+					<div className={styles.skip4}></div>
+
+					<p className={`${styles.textRight} ${styles.text4}`}>
+						{t("step4")}
+					</p>
+					<div className={styles.line}></div>
+					<p className={styles.textAdditional}>*{t("note")}</p>
+				</div>
+			</div>
 		</div>
 	);
+
+	/* версия с флексами */
+
+	// return (
+	// 	<div className="section">
+	// 		<div className="container">
+	// 			<Title title={t("title")} />
+
+	// 			<div className={styles.box}>
+	// 				<div className={styles.boxLeft}>
+	// 					<p>
+	// 						Обговорення технічного завдання, укладання договору
+	// 						та передоплата
+	// 					</p>
+	// 				</div>
+	// 				<div
+	// 					className={`${styles.boxRight} ${styles.boxRightPosition}`}
+	// 				>
+	// 					<p>Створення та затвердження дизайну</p>
+	// 					{/* <div className={styles.box2}></div>
+	// 					<div className={styles.box3}></div> */}
+	// 				</div>
+	// 				<div
+	// 					className={`${styles.boxLeft} ${styles.boxLeftPosition}`}
+	// 				>
+	// 					<p>Розробка сайту</p>
+	// 				</div>
+	// 				<div className={styles.boxRight}>
+	// 					<p className={styles.lastText}>
+	// 						Схвалення клієнта на розміщення сайту в інтернеті
+	// 						<span>
+	// 							*За необхідності додаткові правки на будь-якому
+	// 							етапі
+	// 						</span>
+	// 					</p>
+	// 				</div>
+	// 				<div className={styles.line}></div>
+	// 			</div>
+	// 		</div>
+	// 	</div>
+	// );
 };
 
 export default Steps;
